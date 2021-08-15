27d16f71df312ff0420b7dd78a24232412065ca1
mariuszsak <mariuszsak@gmail.com>
2021-08-15 22:08:07 +0200

    WIP

---
+++ apps/web/src/components/FetchData.vue
@@ -0,0 +1,26 @@
+<template>
+  <h3>{{ val }}</h3>
+</template>
+
+<script>
+
+export default {
+  name: 'FetchData',
+  data () {
+    return {
+      val: null
+    }
+  },
+  mounted () {
+    fetch('http://localhost:3000/countall')
+      .then(res => res.json())
+      .then(data => {
+        this.val = data
+      })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ apps/web/src/views/Status.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Status page</h1>
+    <FetchData/>
+  </div>
+</template>
+
+<script>
+import FetchData from '../components/FetchData'
+
+export default {
+  name: 'Status',
+  components: { FetchData }
+}
+</script>
+
+<style scoped>
+
+</style>
