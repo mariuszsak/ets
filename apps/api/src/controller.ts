afb501d07f6b565693dd1950b1c047bd97479e41
mariuszsak <mariuszsak@gmail.com>
2021-08-28 20:59:25 +0200

    Endpoint added

--- apps/api/src/controller.ts
+++ apps/api/src/controller.ts
@@ -50,15 +50,43 @@ router.get('/issues', async (req: express.Request, res) => {
 );
 
 
+router.get('/issue/list', async (req: express.Request, res) => {
+        try {
+            const tick = await prisma.ticket.findMany({
+                where: {
+                    OR: [
+                        {
+                            status: 'REGISTERED'
+                        },
+                        {
+                            status: 'INPROGRESS'
+                        }
+                    ]
+                }
+            });
+            res
+                .status(200)
+                .send(JSON.stringify(tick))
+        } catch (err) {
+            res
+                .status(400)
+                .send(JSON.stringify(err))
+                .end();
+            console.log(err)
+        }
+    }
+);
+
 router.post('/issue/addissue', async (req: express.Request, res) => {
-        const {registeredAt, authorId, solvedAt} = req.body.data;
+        const {registeredAt, authorId, solvedAt, issue} = req.body.data;
         console.log(req.body);
         try {
             const tick = await prisma.ticket.create({
                 data: {
                     registeredAt: new Date(registeredAt),
                     authorId: authorId,
-                    solvedAt: new Date(solvedAt)
+                    solvedAt: new Date(solvedAt),
+                    issue: issue
                 }
             });
             res
