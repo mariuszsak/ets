8e3202b1272caeb517730522818ff9d9cee14fd2
mariuszsak <mariuszsak@gmail.com>
2021-09-09 01:18:21 +0200

    Component added

---
+++ apps/web/src/components/Logout.vue
@@ -0,0 +1,28 @@
+<template>
+  <router-link to="/" @click="Logout">Logout</router-link>
+</template>
+
+<script>
+import firebase from 'firebase/compat'
+
+export default {
+  name: 'Logout',
+  setup () {
+    const Logout = () => {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => console.log('User logged out'))
+        .catch(err => console.warn(err.message))
+    }
+
+    return {
+      Logout
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
