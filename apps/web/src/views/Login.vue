a1aaea4c7ab3af4ce80cdc8b4d43bfa7cccf4b1b
mariuszsak <mariuszsak@gmail.com>
2021-09-08 17:39:46 +0200

    WIP

--- apps/web/src/views/Login.vue
+++ apps/web/src/views/Login.vue
@@ -1,25 +1,34 @@
 <template>
   <h1> Login to Your Account </h1>
-  <p> <input type='text' placeholder="Email" v-model='email'/> </p>
-  <p> <input type='password' placeholder="Password" v-model='password'/> </p>
-  <p> <button @click="signIn"> Submit </button> </p>
+  <p><input type='text' placeholder='Email' v-model='email'/></p>
+  <p><input type='password' placeholder='Hasło' v-model='password'/></p>
+  <p>
+    <button @click='Login'> Submit</button>
+  </p>
 </template>
 
 <script>
+import { ref } from 'vue'
+import firebase from 'firebase/compat'
 
 export default {
-  name: 'Login',
-  data () {
-    return {
-      val: null
+  setup () {
+    const email = ref('')
+    const password = ref('')
+
+    const Login = () => {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(email.value, password.value)
+        .then(data => console.log(data))
+        .catch(err => alert(err.message))
     }
-  },
-  mounted () {
-    fetch('http://localhost:3000/issue/list')
-      .then(res => res.json())
-      .then(data => {
-        this.val = data
-      })
+
+    return {
+      Login,
+      email,
+      password
+    }
   }
 }
 </script>
