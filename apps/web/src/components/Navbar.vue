3511e6cc6a97b330aa75dc4b300891d4fa1e50e4
mariuszsak <mariuszsak@gmail.com>
2021-09-08 23:46:53 +0200

    Navigation moved to component

---
+++ apps/web/src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/status">Status</router-link> |
+    <router-link to="/mainform">MainForm</router-link>
+    <!--      <router-link to="/login">Login</router-link>-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style scoped>
+
+</style>
