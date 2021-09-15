9689f8baf8d99d89747704066fc7c4b401d4353c
mariuszsak <mariuszsak@gmail.com>
2021-09-15 23:44:55 +0200

    Endpoint added, WIP

--- apps/api/src/controller.ts
+++ apps/api/src/controller.ts
@@ -1,8 +1,11 @@
 import {PrismaClient} from "@prisma/client";
 import express from 'express';
+import jwt from 'jsonwebtoken';
+
 
 const prisma = new PrismaClient();
 const router = express.Router();
+
 
 router.get('/users', async (req: express.Request, res: express.Response) => {
         const {email} = req.body.data;
@@ -18,6 +21,33 @@ router.get('/users', async (req: express.Request, res: express.Response) => {
                 .status(200)
                 .send(JSON.stringify(user.role))
                 .end();
+        } catch (err) {
+            res
+                .status(400)
+                .send(JSON.stringify(err))
+                .end();
+        }
+    }
+);
+
+router.post('/login', async (req: express.Request, res: express.Response) => {
+        const {email} = req.body.data;
+        try {
+            const user = await prisma.user.findFirst({
+                where: {
+                    email: email
+                }
+            });
+            if (!user) {
+                return res
+                    .status(401)
+                    .send('Unauthorized');
+            }
+
+            const payload = user;
+            const token = jwt.sign(payload, process.env.ACCESS_TOKEN)
+
+            res.json({token});
         } catch (err) {
             res
                 .status(400)
