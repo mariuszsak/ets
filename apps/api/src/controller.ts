b89c33a0f591ecd000f49b1a44b0e48bbfa1ebb2
mariuszsak <mariuszsak@gmail.com>
2021-09-15 23:52:46 +0200

    Authentication middleware added, WIP

--- apps/api/src/controller.ts
+++ apps/api/src/controller.ts
@@ -6,6 +6,20 @@ import jwt from 'jsonwebtoken';
 const prisma = new PrismaClient();
 const router = express.Router();
 
+const authMiddleware = (req, res, next) => {
+    const token = req.headers['authorization']?.split(' ')[1]
+    if (!token) {
+        return res.status(401).send('unauthorized');
+    }
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, (error, data) => {
+        if (error) {
+            res.status(403).send('Forbidden')
+        }
+        req.user = data;
+        next();
+    })
+}
 
 router.get('/users', async (req: express.Request, res: express.Response) => {
         const {email} = req.body.data;
