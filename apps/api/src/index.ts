47d15241aa6f0bcf3b4527186417a10146df11f4
mariuszsak <mariuszsak@gmail.com>
2021-09-15 23:45:47 +0200

    Dotenv config included

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,9 +2,12 @@ import {PrismaClient} from '@prisma/client';
 import express from 'express';
 import cors from 'cors';
 import controller from './controller';
+import dotenv from 'dotenv';
 
 export const prisma = new PrismaClient();
 export const app = express();
+
+dotenv.config();
 
 app.use(cors({
     origin: '*'
